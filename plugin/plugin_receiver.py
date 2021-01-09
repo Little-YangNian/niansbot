4c8a9d0e82e931a2520a738bbd0c9cd5ae1512c5
YangNian <65386393+ChaseOnion@users.noreply.github.com>
2021-01-09 08:13:44 +0800

    Add files via upload

---
+++ plugin/plugin_receiver.py
@@ -0,0 +1,22 @@
+from apps.app import bcc,app
+from graia.application.message.chain import MessageChain
+from graia.application.group import Group,Member
+from graia.application.message.elements.internal import Plain,At,Image
+from graia.application import GraiaMiraiApplication
+from plugin.plugins.rua import ruaer
+import re
+
+@bcc.receiver('GroupMessage')
+async def rua(app: GraiaMiraiApplication,member:Member,message:MessageChain,group:Group):
+    if message.asDisplay().startswith('rua'):
+    #output = await ruaer(member.id)
+        b = message.get(At)
+        if b :
+        
+            print(b[0])
+            c = str(b[0])
+            res = re.findall('target=.*?(.*?) display=',c)
+            print(int(res[0]))
+            url =  ruaer(int(res[0]))
+            print(url)
+            await app.sendGroupMessage(group,MessageChain.create([Image.fromLocalFile(url)]))
