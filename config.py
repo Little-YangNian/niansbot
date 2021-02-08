3123858df4460a2accac6f2644dce9f0543bcf25
Chase杨念 <65386393+Little-YangNian@users.noreply.github.com>
2021-02-08 17:05:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from miraibot.default_config import *
 HOST: str = 'localhost' # 必填
 PORT: str = 8080  # 必填 
 DEBUG: str = False
-AUTHKEY: str = 'qaq1940QAQ' # 如果没有设置则 mirai http api 会随机生成它，请自行从控制台或 mirai http api 的配置文件中获取
+AUTHKEY: str = 'owo' # 如果没有设置则 mirai http api 会随机生成它，请自行从控制台或 mirai http api 的配置文件中获取
 
 QQ: int = 1261785144  # 必填
