43b9a7d782499e8d42353445f3d3206d354d3535
YangNian <2544704967@qq.com>
2021-02-09 15:37:08 +0800

    qwq

--- plugins/McTool.py
+++ plugins/McTool.py
@@ -1,13 +1,16 @@
 import aiohttp
+import aiofiles
 from miraibot import get
 from miraibot import GraiaMiraiApplication
-from miraibot.message import MessageChain, Group, Member
+from miraibot.message import MessageChain, Image,  Group, Member, Plain
 import json
+import base64
+import os
 
 commands  =  ['ft','在吗']
-groups = [12345678]
-hosts = {}
-# Group:Host
+groups = [488467276]
+
+host = "cn-xz-bgp.sakurafrp.com:23300"
 
 bcc = get.bcc()
 
@@ -17,6 +20,19 @@ async def mc_tool(msg: MessageChain, group: Group, member: Member,app: GraiaMira
     ctx = msg.asDisplay()
     if ctx in commands and group.id in groups:
         
-        async with aiohttp.request('get',f'https://api.bluesdawn.top/minecraft/server/api?host={hosts[group.id]}') as rest:
-            respon  = await rest.text()
+        async with aiohttp.request('get',f'https://api.bluesdawn.top/minecraft/server/api?host={host}') as rest:
+            respon  = await rest.read()
+            res = json.loads(respon)
+            code = res["favicon"]
+            player = ""
+            favicon = base64.b64decode((code.replace(r"data:image/png;base64,","")))
+            for i in res['players']['list']:
+                player = player + i['name'] + '\n'
+            async with aiofiles.open("fav.png",mode="wb") as f:
+                await f.write(favicon)
+            result = f"服务器查询:\n状态:{res['status']}\n{res['version']['version']}\n在线玩家:{player}\n"
+            chain = msg.create(
+                    [Plain(result),Image.fromLocalFile("./fav.png")]
+                    )
+            await app.sendGroupMessage(group,chain)
         
